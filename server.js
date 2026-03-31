b03d973617582c4af073ff0f2c6581793d34c6aa
sankyoconnectvnjp-sys <sankyo.connect.vnjp@gmail.com>
2026-03-31 14:39:33 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.get("/sales", (req, res) => res.sendFile(path.join(__dirname, "sales.html"))
 app.get("/inventory", (req, res) => res.sendFile(path.join(__dirname, "inventory.html")));
 app.get("/debt", (req, res) => res.sendFile(path.join(__dirname, "debt.html")));
 app.get("/report", (req, res) => res.sendFile(path.join(__dirname, "report.html")));
+app.get("/suppliers", (req, res) => res.sendFile(path.join(__dirname, "suppliers.html")));
 
 app.get("*", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
